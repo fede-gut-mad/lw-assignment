ccbb3103679cef63f208187e9b3f564f2d3d82c1
Federico Gutierrez <fgm.1989@gmail.com>
2025-06-28 01:38:35 +0200

    remove comments

--- playwright.config.ts
+++ playwright.config.ts
@@ -63,11 +63,4 @@ export default defineConfig({
         ...devices['Pixel 5'] },
     },
   ],
-
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://localhost:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
 });
